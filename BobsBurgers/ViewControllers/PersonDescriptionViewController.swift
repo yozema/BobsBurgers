84d9161663b7fc8fb9c2e91b6031dc342f70bae8
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-28 17:19:48 -0300

    Person manual parsing

---
+++ BobsBurgers/ViewControllers/PersonDescriptionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterDescriptionViewController.swift
+//  BobsBurgers
+//
+//  Created by Ilya Zemskov on 28.03.2023.
+//
+
+import UIKit
+
+class PersonDescriptionViewController: UIViewController {
+    @IBOutlet var personImage: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
---
+++ BobsBurgers/ViewControllers/PersonsCollectionViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CharactersCollectionViewController.swift
+//  BobsBurgers
+//
+//  Created by Ilya Zemskov on 28.03.2023.
+//
+
+import UIKit
+
+class PersonsCollectionViewController: UICollectionViewController {
+    // MARK: - Properties
+    private var persons: [Person] = []
+    private var networkManager = NetworkManager.shared
+    private var spinnerView = UIActivityIndicatorView()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showSpinner(in: view)
+        fetchPersons()
+    }
+    
+    // MARK: - Private Functions
+    private func fetchPersons() {
+        networkManager.fetchPersons(from: Link.BobsBurgers.url) { [weak self] result in
+            switch result {
+            case .success(let persons):
+                self?.persons = persons
+                self?.collectionView.reloadData()
+                self?.spinnerView.stopAnimating()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    private func showSpinner(in view: UIView) {
+        spinnerView = UIActivityIndicatorView(style: .large)
+        spinnerView.color = .red
+        spinnerView.startAnimating()
+        spinnerView.center = view.center
+        spinnerView.hidesWhenStopped = true
+        
+        view.addSubview(spinnerView)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension PersonsCollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        persons.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "personCell", for: indexPath)
+        guard let cell = cell as? PersonCell else { return UICollectionViewCell() }
+        let character = persons[indexPath.item]
+        cell.configure(with: character)
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedCharacter = persons[indexPath.item]
+        performSegue(withIdentifier: "showDescription", sender: selectedCharacter)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension PersonsCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 30, height: 100)
+    }
+}
